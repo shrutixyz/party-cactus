6880c8625ada35cbdd0c4a1a4d0e972cd2e45203
shruti <shrutigupta5555@gmail.com>
2022-05-01 14:29:15 +0530

    temsting

--- js/sound.js
+++ js/sound.js
@@ -1,13 +1,20 @@
 
+const aakash = document.querySelector(".aakash")
 
 const player = new Tone.Player({
 
-	url: "https://shrutigupta5555.github.io/party-cactus/sample.mp3",
-	autostart: true,
+    url: "https://shrutigupta5555.github.io/party-cactus/sample.mp3",
+    autostart: true
+    
 });
-const filter = new Tone.Filter(400, 'lowpass').toDestination();
-const feedbackDelay = new Tone.FeedbackDelay(0.125, 0.5).toDestination();
+aakash.addEventListener("click", e => {
+    console.log("click")
+    
+    const filter = new Tone.Filter(400, 'lowpass').toDestination();
+    const feedbackDelay = new Tone.FeedbackDelay(0.125, 0.5).toDestination();
+    
+    // connect the player to the feedback delay and filter in parallel
+    player.connect(filter);
+    player.connect(feedbackDelay);
+})
 
-// connect the player to the feedback delay and filter in parallel
-player.connect(filter);
-player.connect(feedbackDelay);
