d1f6cb831b8c7d23d932c194b6e224ca335ecf76
shruti <shrutigupta5555@gmail.com>
2022-05-01 14:22:16 +0530

    audio

--- js/sound.js
+++ js/sound.js
@@ -1,7 +1,8 @@
 
 
 const player = new Tone.Player({
-	url: "https://cdn.discordapp.com/attachments/873911460055642152/970242747460104232/Kanye_Gave_Kim_1000000_TubeRipper.com.mp3",
+
+	url: "https://shrutigupta5555.github.io/party-cactus/sample.mp3",
 	autostart: true,
 });
 const filter = new Tone.Filter(400, 'lowpass').toDestination();
